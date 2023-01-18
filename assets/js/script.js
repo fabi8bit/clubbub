6c5b26c96ccfa163c9962f443a5d21a437b5b337
fabi8bit <fabi8bit@gmail.com>
2023-01-18 20:30:46 +0000

    Add event listener for all the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,32 @@
+// Wait for the DOM to finish loading
+// Add event listeners to the buttons
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByClassName('buttons');
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("id") === "play") {
+                alert(`You clicked ${this.getAttribute("id")}!`);
+            } else {
+                let buttonType = this.getAttribute("id");
+                alert(`You clicked ${buttonType}`);
+            }
+        });
+    }
+});
+
+
 function setupStory() {
 
 }
 
 function buttonScroll() {
+    
+
+}
+
+function shuffle() {
 
 }
 
