350cf27a4e17f844c25b76c362da0a48506338f9
fabi8bit <fabi8bit@gmail.com>
2023-01-18 22:12:27 +0000

    Add the js code to set the initial state for the button of the choice area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
 // Wait for the DOM to finish loading
+// Load the array with the favicons HTML code
 // Add event listeners to the buttons
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByClassName('buttons');
+    let heroArr = ['<i class="fa-solid fa-kiwi-bird">','<i class="fa-solid fa-mask"></i>','<i class="fa-solid fa-user-astronaut"></i>'];
+    let genreArr = ['<i class="fa-regular fa-hand-peace"></i>','<i class="fa-solid fa-bolt-lightning"></i>','<i class="fa-solid fa-hat-wizard"></i>'];
+    let environmentArr = ['<i class="fa-solid fa-rocket"></i>','<i class="fa-solid fa-tree"></i>','<i class="fa-solid fa-city"></i>'];
+    let selection = [heroArr,genreArr,environmentArr];
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
@@ -10,10 +15,20 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert(`You clicked ${this.getAttribute("id")}!`);
             } else {
                 let buttonType = this.getAttribute("id");
-                alert(`You clicked ${buttonType}`);
+                buttonScroll(buttonType);
             }
         });
+
     }
+//Set initial state for the button of the choice-area
+    let choices = document.getElementsByClassName('choice');
+    console.log(choices[0].innerHTML);
+    for (let i=0 ; i<choices.length ; i++) {
+        choices[i].innerHTML = selection[i][0];
+        
+    }
+
+
 });
 
 
@@ -21,7 +36,7 @@ function setupStory() {
 
 }
 
-function buttonScroll() {
+function buttonScroll(buttonType) {
     
 
 }
