31053b2f54fd81514eeb1db0ca30466d7c966c80
fabi8bit <fabi8bit@gmail.com>
2023-02-02 15:08:53 +0000

    improve use of classes in html, improve css, correct javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,26 @@
-let heroArr = ['<i class="fa-solid fa-cat"> Animal</i>','<i class="fa-solid fa-mask"> Super Hero</i>','<i class="fa-solid fa-user-astronaut"> Human</i>'];
-let genreArr = ['<i class="fa-regular fa-hand-peace"> Friendship</i>','<i class="fa-solid fa-bolt-lightning"> War</i>','<i class="fa-solid fa-hat-wizard"> Magic</i>'];
-let environmentArr = ['<i class="fa-solid fa-rocket"> Space</i>','<i class="fa-solid fa-tree"> Nature</i>','<i class="fa-solid fa-city"> City</i>'];
-let selection = [heroArr,genreArr,environmentArr];
+let heroArr = ['<i class="fa-solid fa-cat"> Animal</i>', '<i class="fa-solid fa-mask"> Super Hero</i>', '<i class="fa-solid fa-user-astronaut"> Human</i>'];
+let genreArr = ['<i class="fa-regular fa-hand-peace"> Friendship</i>', '<i class="fa-solid fa-bolt-lightning"> War</i>', '<i class="fa-solid fa-hat-wizard"> Magic</i>'];
+let environmentArr = ['<i class="fa-solid fa-rocket"> Space</i>', '<i class="fa-solid fa-tree"> Nature</i>', '<i class="fa-solid fa-city"> City</i>'];
+let selection = [heroArr, genreArr, environmentArr];
 
 
 // Wait for the DOM to finish loading
 // Load the array with the favicons HTML code
 // Add event listeners to the buttons
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByClassName('buttons');
     for (let button of buttons) {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             if (this.getAttribute("id") === "play") {
                 createStory();
-                
-                 
+
+
             } else if (this.getAttribute("id") === "shuffle") {
                 shuffle();
             } else if (this.getAttribute("id") === "reset") {
-                displayStory('initial','none');
-            }
-            else {
+                displayStory('display-all', 'display-none');
+            } else {
                 let buttonType = this.getAttribute("id");
                 buttonScroll(buttonType);
             }
@@ -36,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function() {
 function setInitialState() {
     let choices = document.getElementsByClassName('choice');
     // console.log(choices[0].innerHTML);
-    for (let i=0 ; i<choices.length ; i++) {
+    for (let i = 0; i < choices.length; i++) {
         choices[i].innerHTML = selection[i][0];
     }
 }
@@ -46,12 +45,12 @@ function setInitialState() {
  */
 function shuffle() {
     let choices = document.getElementsByClassName('choice');
-    for (let i=0 ; i<choices.length ; i++) {
+    for (let i = 0; i < choices.length; i++) {
         let randomIndex = Math.floor(Math.random() * 3);
         console.log(choices[i]);
         console.log(randomIndex);
         choices[i].innerHTML = selection[i][randomIndex];
-        choices[i].setAttribute('data-selector',randomIndex);
+        choices[i].setAttribute('data-selector', randomIndex);
     }
 }
 
@@ -70,13 +69,13 @@ function buttonScroll(buttonType) {
         thisArr = genreArr;
     } else if (thisButton === environment) {
         thisArr = environmentArr;
-    } 
+    }
     parseInt(dataSelector++);
-    if (dataSelector > thisArr.length-1){
+    if (dataSelector > thisArr.length - 1) {
         dataSelector = 0;
-        thisButton.setAttribute('data-selector',dataSelector);
+        thisButton.setAttribute('data-selector', dataSelector);
     } else {
-        thisButton.setAttribute('data-selector',dataSelector); // setAttribute from this tutorial https://www.javascripttutorial.net/dom/attributes/set-the-value-of-an-attribute/
+        thisButton.setAttribute('data-selector', dataSelector); // setAttribute from this tutorial https://www.javascripttutorial.net/dom/attributes/set-the-value-of-an-attribute/
     }
     thisButton.innerHTML = thisArr[dataSelector];
 }
@@ -89,19 +88,44 @@ function createStory() {
     let storyId = "s";
     let choices = document.getElementsByClassName('choice');
     // console.log(choices[0].innerHTML);
-    for (let i=0 ; i<choices.length ; i++) {
+    for (let i = 0; i < choices.length; i++) {
         console.log(choices[i].getAttribute('data-selector'));
         storyId += choices[i].getAttribute('data-selector');
         console.log(storyId);
     }
-    displayStory('none','initial');
+    displayStory('display-none', 'display-all');
     //put all the stories invisible before the selected one (storyId) will be set to visible 
     let stories = document.getElementsByClassName('stories');
-    for (let i=0 ; i<stories.length ; i++) {
-        stories[i].style.display = 'none';
+    for (let i = 0; i < stories.length; i++) {
+        
+        stories[i].classList.add("display-none");
     }
     //the selected story is set to visible ('initial')
-    document.getElementById(storyId).style.display = 'initial';
+    document.getElementById(storyId).classList.remove("display-none");
+    document.getElementById(storyId).classList.add("display-all");
+
+//     document.getElementById(storyId).style.display = 'initial';
+//     var style = document.createElement('style');
+//     style.innerHTML = `
+//     #story-area {
+//         background-image: url('../assets/images/pattMagic.webp'); 
+//     width: 85%;
+//     max-width: 850px;
+//     margin-top: 20px;
+//     margin-left: auto;
+//     margin-right: auto;
+//     margin-bottom: 30px;
+//     border-radius: 20px;
+//     height: 50vh;
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: center;
+        
+//     }
+//     `;
+//     document.head.appendChild(style);
+
+
 }
 
 /**
@@ -109,10 +133,14 @@ function createStory() {
  * @param {set the wizard buttons visible or invisible} dispWiz 
  * @param {set the Story visible or invisible} dispStory 
  */
-function displayStory(dispWiz,dispStory) {
-    
-    document.getElementById("wizard-area").style.display = dispWiz;
-    document.getElementById("story-area").style.display = dispStory;
+function displayStory(dispWiz, dispStory) {
+    document.getElementById("wizard-area").classList.remove(dispWiz);
+    document.getElementById("wizard-area").classList.remove(dispStory);
+    document.getElementById("story-area").classList.remove(dispWiz);
+    document.getElementById("story-area").classList.remove(dispStory);
+    document.getElementById("wizard-area").classList.add(dispWiz);
+    document.getElementById("story-area").classList.add(dispStory);
+    // document.getElementById("wizard-area").style.display = dispWiz;
+    // document.getElementById("story-area").style.display = dispStory;
     // document.getElementById(storyId).style.display = 'initial';
 }
-
